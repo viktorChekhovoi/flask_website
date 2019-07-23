8f96ab39ebaf779c29857aa4ce56f9c19528db67
victorChekhovoy <victor.chekhovoy@gmail.com>
2019-07-23 19:26:12 +0000

    creating templates

--- flaskey/firstapp/routes.py
+++ flaskey/firstapp/routes.py
@@ -1,6 +1,8 @@
 from firstapp import app
-
+from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return "henlo!"
+    username = 'pictor'
+    return render_template('index.html', user=username)
+
