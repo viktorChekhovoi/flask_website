97c40814a7157926573227fed99db93924c91702
victorChekhovoy <victor.chekhovoy@gmail.com>
2019-07-28 14:10:54 +0000

    login now works

--- flaskey/firstapp/login_form.py
+++ flaskey/firstapp/login_form.py
@@ -1,9 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from firstapp.models import User
 
 class Login_Form(FlaskForm):
     username = StringField('username', validators = [DataRequired()])
     password = PasswordField('password', validators = [DataRequired()])
     remember_me = BooleanField('remember me')
     sign_in = SubmitField('sign in')
+
+
+class Registration_Form(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.') 
--- flaskey/firstapp/routes.py
+++ flaskey/firstapp/routes.py
@@ -1,11 +1,14 @@
-from firstapp import app
-from flask import render_template, redirect, flash, url_for
-from firstapp.login_form import Login_Form
+from firstapp import app, db
+from flask import render_template, redirect, flash, url_for, request
+from firstapp.login_form import Login_Form, Registration_Form
 import requests
 import json
-
+from flask_login import current_user, login_user, logout_user, login_required
+from firstapp.models import User
+from werkzeug.urls import url_parse
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     url = "https://api.github.com/users/victorChekhovoy/events"
     r = requests.get(url)
@@ -15,15 +18,45 @@ def index():
         if a['type'] == 'PushEvent':
             last = a
             break
-    username = 'pictor'
-    return render_template('index.html', user=username, date = last['created_at'])
+
+    return render_template('index.html', date = last['created_at'])
 
 
 @app.route('/login', methods = ['GET', 'POST'])
-def login():
+def login(): 
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = Login_Form()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me = {}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('invalid username or password')
+            return redirect(url_for('login'))
+
+        login_user(user, remember = form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+
     return render_template('login.html',title = 'sign in',  login_form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/registration', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = Registration_Form()
+    if form.validate_on_submit():
+        user = User(username = form.username.data, email = form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('registration.html', form = form)
